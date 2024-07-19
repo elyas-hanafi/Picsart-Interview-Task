89319776596b07f58012a7c1915adbd5e6672a27
elyas-hanafi <eliinsta79@gmail.com>
2024-07-19 20:37:03 +0330

    feat: Display error message in ErrorBoundary

--- src/components/error-boundary/index.tsx
+++ src/components/error-boundary/index.tsx
@@ -6,16 +6,18 @@ interface Props {
 
 interface State {
   hasError: boolean;
+  errorMessage: string;
 }
 
 class ErrorBoundary extends Component<Props, State> {
   public state: State = {
     hasError: false,
+    errorMessage: "",
   };
 
-  public static getDerivedStateFromError(_: Error): State {
+  public static getDerivedStateFromError(error: Error): State {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { hasError: true, errorMessage: error.message };
   }
 
   public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
@@ -24,7 +26,7 @@ class ErrorBoundary extends Component<Props, State> {
 
   public render() {
     if (this.state.hasError) {
-      return <h1>Sorry.. there was an error</h1>;
+      return <h1>Sorry.. there was an error: {this.state.errorMessage}</h1>;
     }
 
     return this.props.children;
